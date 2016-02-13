dd9db02423b7478ef757427040ecc2c60dc51fc0
Take Haru <sj0030rx@gmail.com>
2016-02-13 17:01:58 +0900

    first commit

---
+++ wordTech/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  wordTech
+//
+//  Created by TakeuchiHaruki on 2016/02/13.
+//  Copyright © 2016年 Haruki. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
